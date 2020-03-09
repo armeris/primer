752820e92d69d72907de9aa2b6e71794a9024681
Rubén Salinas <ruben.salinas@edreamsodigeo.com>
2020-03-09 17:19:41 +0100

    [PRIMER] First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./example";
+
+export default class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 4
+        };
+    }
+
+    isEven(val) {
+        return val % 2 === 0 ? "Even" : "Odd";
+    }
+
+    getClassName(val) {
+        return val % 2 === 0
+            ? "bg-primary text-white text-center p-2 m-1"
+            : "bg-secondary text-white text-center p-2 m-1";
+    }
+
+    handleClick = () => this.setState({ count: this.state.count + 1 });
+
+    render = () =>
+        <div className="m-2">
+            <div className="form-group">
+                <label>Name:</label>
+                <input className="form-control" />
+            </div>
+            <div className="form-group">
+                <label>City:</label>
+                <input className="form-control" />
+            </div>
+        </div>
 }
-
-export default App;
