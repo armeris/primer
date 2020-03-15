78356b6bb6d4c888a11172496467f02a1215e32c
Rubén <rubensalinasgarcia@protonmail.com>
2020-03-15 18:09:10 +0100

    Added objects code

--- src/example.js
+++ src/example.js
@@ -6,3 +6,50 @@ console.log("This is also a statement");
 let myArray = [100, "Rubén", true];
 
 myArray.forEach((val, index) => console.log(val));
+
+let myData = {
+    name: "Valeria",
+    weather: "sunny",
+    printMessages: () => {
+        console.log(`Hello ${myData.name}`);
+        console.log(`Today is ${myData.weather}`);
+    }
+}
+
+myData.printMessages();
+
+class MyData {
+    constructor () {
+        this.name = "Rubén";
+        this.weather = "sunny";
+    }
+
+    printMessages = () => {
+        console.log(`>>> Hello ${this.name}`);
+        console.log(`>>> Today is ${this.weather}`);
+    }
+}
+
+let myData2 = new MyData();
+myData2.printMessages();
+
+let secondObject = { ...myData2, weather: "cloudy" };
+console.log(`myData: ${myData2.weather}, secondObject: ${secondObject.weather}`);
+
+const myData3 = {
+    name: "Bob",
+    location: {
+        city: "Paris",
+        country: "France"
+    },
+    employment: {
+        title: "Manager",
+        dept: "Sales"
+    }
+}
+
+function printDetails({ name, location: {city}, employment: {title} }) {
+    console.log(`Name: ${name}, City: ${city}, Role: ${title}`);
+}
+
+printDetails(myData3);
