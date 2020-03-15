78d82035370b7f54385c8e82b294e8cb4a1efc47
Rubén <rubensalinasgarcia@protonmail.com>
2020-03-15 17:49:02 +0100

    Added array examples

--- src/example.js
+++ src/example.js
@@ -2,3 +2,7 @@ console.log("Hello");
 console.log("Apples");
 console.log("This is a statement");
 console.log("This is also a statement");
+
+let myArray = [100, "Rubén", true];
+
+myArray.forEach((val, index) => console.log(val));
